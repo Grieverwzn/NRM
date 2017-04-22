6aa4be53d04757fcdf885c0b454eaa98df65593f
Wuyang Yuan <wyyuan1991@foxmail.com>
2017-04-22 11:11:04 +0800

    2017年4月22日11:11:01

---
+++ Solver/SimSolver/SimSolver.cs
@@ -0,0 +1,16 @@
+﻿using com.foxmail.wyyuan1991.NRM.RailwayModel;
+using com.foxmail.wyyuan1991.NRM.Simulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimSolver
+{
+    public interface SimSolver
+    {
+        BookingSimulator Simulator { get; set; }
+        NRMDataAdapter Data { get; set; }
+
+    }
+}
